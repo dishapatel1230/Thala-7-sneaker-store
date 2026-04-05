fb6f473711f09e2c40bb34b1de214be00d3b90b3
dishapatel1230 <dishapatel3012@gmail.com>
2026-04-05 21:08:55 +0530

    update header and responsiv style

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function changeImage(el) {
   document.getElementById("main-image").src = el.src;
 }
+
+/*menu */
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault(); // prevents weird behavior
+  menu.classList.toggle("active");
+});
+
+/*close menu when click link */
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
